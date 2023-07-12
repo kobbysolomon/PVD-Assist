d279dca57f63429a301748b4ceb0339dd9b5b849
Godfried Solomon(BHL5GVK) <gsolomon@ups.com>
2023-07-12 16:52:39 -0400

    dependencies installed

--- PVD-Titan/angularapp/src/app/app.module.ts
+++ PVD-Titan/angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
