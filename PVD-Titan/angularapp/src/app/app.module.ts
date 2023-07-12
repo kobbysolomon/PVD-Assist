c11430e48287335b53730800b331d986e69804c9
Godfried Solomon(BHL5GVK) <gsolomon@ups.com>
2023-07-12 19:09:52 -0400

    lucy-godfried-merged

--- PVD-Titan/angularapp/src/app/app.module.ts
+++ PVD-Titan/angularapp/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+
+];
+
 
 @NgModule({
   declarations: [
@@ -15,8 +24,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
+
   providers: [],
   bootstrap: [AppComponent]
 })
