8f4f1fbb1b0cf122263e7b33316ad8d0406249e8
Shaleen Parikh (NXP5VRF) <sparikh@ups.com>
2023-07-12 19:29:01 -0400

    database

--- PVD-Titan/webapi/CarDBContext.cs
+++ PVD-Titan/webapi/CarDBContext.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Xml;
 
 namespace webapi
 {
     public class CarDBContext: DbContext
     {
         public CarDBContext(DbContextOptions<CarDBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().ToTable("PVD");
+            modelBuilder.Entity<PackageDefault>().HasNoKey().ToTable("PackageDimensions");
+            modelBuilder.Entity<VehicleType>().HasNoKey().ToTable("VehicleDimensions");
+        }
         public DbSet<PackageDefault> PackageDefaults { get; set; }
         public DbSet<Car> Cars { get; set; }
         public DbSet<VehicleType> VehicleTypes { get; set; }
--- PVD-Titan/webapi/Program.cs
+++ PVD-Titan/webapi/Program.cs
@@ -1,13 +1,25 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 using webapi;
 
 var builder = WebApplication.CreateBuilder(args);
+string connectionString = builder.Configuration.GetConnectionString("myDb1");
+
+DbContextOptionsBuilder<CarDBContext> optionsBuilder = new DbContextOptionsBuilder<CarDBContext>();
 
 // Add services to the container.
-builder.Services.AddDbContext<CarDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("myDb1")));
+//builder.Services.AddDbContext<CarDBContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("myDb1")));
+optionsBuilder.UseSqlServer(connectionString);
+
+using (CarDBContext context = new CarDBContext(optionsBuilder.Options))
+{
+    var packageDefaults = context.PackageDefaults.ToList();
+    var pvds = context.Cars.ToList();
+    var vehicleTypes = context.VehicleTypes.ToList();
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
