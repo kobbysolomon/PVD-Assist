fc69225aa37050c171d8d3d07f87daa6a8a31cf6
Shaleen Parikh (NXP5VRF) <sparikh@ups.com>
2023-07-13 08:48:31 -0400

    added formcontroller, found assignedPackages list

--- PVD-Titan/webapi/Car.cs
+++ PVD-Titan/webapi/Car.cs
@@ -6,7 +6,7 @@
         public int Year_of_manufacture { get;set;}
         public string? Vehicle_make { get;set;}
         public string? Vehicle_model { get;set;}
-        public string? Vehicle_type { get; set; }
+        public string? Car_type { get; set; }
         public int Vehicle_cargo_space { get; set; }
         public int Pv { get; set; }
         public int Lv { get; set; }
